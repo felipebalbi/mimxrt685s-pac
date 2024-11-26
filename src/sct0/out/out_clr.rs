c5a16ed0b3369784b6d214bea5398dd5a0a72d58
Felipe Balbi <felipe@balbi.sh>
2024-11-26 06:02:04 +0200

    More interesting updates to the SVD file
    Add enumerated values for CRC polynomials
    
    Create accessors for each bit in SCT OUT and CLR registers.
    
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- src/sct0/out/out_clr.rs
+++ src/sct0/out/out_clr.rs
@@ -2,28 +2,219 @@
 pub type R = crate::R<OutClrSpec>;
 #[doc = "Register `OUT_CLR` writer"]
 pub type W = crate::W<OutClrSpec>;
-#[doc = "Field `CLR` reader - A 1 in bit m selects event m to clear output n (or set it if SETCLRn = 0x1 or 0x2) event 0 = bit 0, event 1 = bit 1, etc. The number of bits = number of events in this SCT. When the counter is used in bi-directional mode, it is possible to reverse the action specified by the output set and clear registers when counting down, See the OUTPUTCTRL register."]
-pub type ClrR = crate::FieldReader<u16>;
-#[doc = "Field `CLR` writer - A 1 in bit m selects event m to clear output n (or set it if SETCLRn = 0x1 or 0x2) event 0 = bit 0, event 1 = bit 1, etc. The number of bits = number of events in this SCT. When the counter is used in bi-directional mode, it is possible to reverse the action specified by the output set and clear registers when counting down, See the OUTPUTCTRL register."]
-pub type ClrW<'a, REG> = crate::FieldWriter<'a, REG, 16, u16>;
+#[doc = "Field `CLR(0-15)` reader - "]
+pub type ClrR = crate::BitReader;
+#[doc = "Field `CLR(0-15)` writer - "]
+pub type ClrW<'a, REG> = crate::BitWriter<'a, REG>;
 impl R {
-    #[doc = "Bits 0:15 - A 1 in bit m selects event m to clear output n (or set it if SETCLRn = 0x1 or 0x2) event 0 = bit 0, event 1 = bit 1, etc. The number of bits = number of events in this SCT. When the counter is used in bi-directional mode, it is possible to reverse the action specified by the output set and clear registers when counting down, See the OUTPUTCTRL register."]
+    #[doc = ""]
+    #[doc = ""]
+    #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CLR0` field.</div>"]
     #[inline(always)]
-    pub fn clr(&self) -> ClrR {
-        ClrR::new((self.bits & 0xffff) as u16)
+    pub fn clr(&self, n: u8) -> ClrR {
+        #[allow(clippy::no_effect)]
+        [(); 16][n as usize];
+        ClrR::new(((self.bits >> n) & 1) != 0)
+    }
+    #[doc = "Iterator for array of:"]
+    #[doc = ""]
+    #[inline(always)]
+    pub fn clr_iter(&self) -> impl Iterator<Item = ClrR> + '_ {
+        (0..16).map(move |n| ClrR::new(((self.bits >> n) & 1) != 0))
+    }
+    #[doc = "Bit 0 - CLR0"]
+    #[inline(always)]
+    pub fn clr0(&self) -> ClrR {
+        ClrR::new((self.bits & 1) != 0)
+    }
+    #[doc = "Bit 1 - CLR1"]
+    #[inline(always)]
+    pub fn clr1(&self) -> ClrR {
+        ClrR::new(((self.bits >> 1) & 1) != 0)
+    }
+    #[doc = "Bit 2 - CLR2"]
+    #[inline(always)]
+    pub fn clr2(&self) -> ClrR {
+        ClrR::new(((self.bits >> 2) & 1) != 0)
+    }
+    #[doc = "Bit 3 - CLR3"]
+    #[inline(always)]
+    pub fn clr3(&self) -> ClrR {
+        ClrR::new(((self.bits >> 3) & 1) != 0)
+    }
+    #[doc = "Bit 4 - CLR4"]
+    #[inline(always)]
+    pub fn clr4(&self) -> ClrR {
+        ClrR::new(((self.bits >> 4) & 1) != 0)
+    }
+    #[doc = "Bit 5 - CLR5"]
+    #[inline(always)]
+    pub fn clr5(&self) -> ClrR {
+        ClrR::new(((self.bits >> 5) & 1) != 0)
+    }
+    #[doc = "Bit 6 - CLR6"]
+    #[inline(always)]
+    pub fn clr6(&self) -> ClrR {
+        ClrR::new(((self.bits >> 6) & 1) != 0)
+    }
+    #[doc = "Bit 7 - CLR7"]
+    #[inline(always)]
+    pub fn clr7(&self) -> ClrR {
+        ClrR::new(((self.bits >> 7) & 1) != 0)
+    }
+    #[doc = "Bit 8 - CLR8"]
+    #[inline(always)]
+    pub fn clr8(&self) -> ClrR {
+        ClrR::new(((self.bits >> 8) & 1) != 0)
+    }
+    #[doc = "Bit 9 - CLR9"]
+    #[inline(always)]
+    pub fn clr9(&self) -> ClrR {
+        ClrR::new(((self.bits >> 9) & 1) != 0)
+    }
+    #[doc = "Bit 10 - CLR10"]
+    #[inline(always)]
+    pub fn clr10(&self) -> ClrR {
+        ClrR::new(((self.bits >> 10) & 1) != 0)
+    }
+    #[doc = "Bit 11 - CLR11"]
+    #[inline(always)]
+    pub fn clr11(&self) -> ClrR {
+        ClrR::new(((self.bits >> 11) & 1) != 0)
+    }
+    #[doc = "Bit 12 - CLR12"]
+    #[inline(always)]
+    pub fn clr12(&self) -> ClrR {
+        ClrR::new(((self.bits >> 12) & 1) != 0)
+    }
+    #[doc = "Bit 13 - CLR13"]
+    #[inline(always)]
+    pub fn clr13(&self) -> ClrR {
+        ClrR::new(((self.bits >> 13) & 1) != 0)
+    }
+    #[doc = "Bit 14 - CLR14"]
+    #[inline(always)]
+    pub fn clr14(&self) -> ClrR {
+        ClrR::new(((self.bits >> 14) & 1) != 0)
+    }
+    #[doc = "Bit 15 - CLR15"]
+    #[inline(always)]
+    pub fn clr15(&self) -> ClrR {
+        ClrR::new(((self.bits >> 15) & 1) != 0)
     }
 }
 #[cfg(feature = "debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        f.debug_struct("OUT_CLR").field("clr", &self.clr()).finish()
+        f.debug_struct("OUT_CLR")
+            .field("clr0", &self.clr0())
+            .field("clr1", &self.clr1())
+            .field("clr2", &self.clr2())
+            .field("clr3", &self.clr3())
+            .field("clr4", &self.clr4())
+            .field("clr5", &self.clr5())
+            .field("clr6", &self.clr6())
+            .field("clr7", &self.clr7())
+            .field("clr8", &self.clr8())
+            .field("clr9", &self.clr9())
+            .field("clr10", &self.clr10())
+            .field("clr11", &self.clr11())
+            .field("clr12", &self.clr12())
+            .field("clr13", &self.clr13())
+            .field("clr14", &self.clr14())
+            .field("clr15", &self.clr15())
+            .finish()
     }
 }
 impl W {
-    #[doc = "Bits 0:15 - A 1 in bit m selects event m to clear output n (or set it if SETCLRn = 0x1 or 0x2) event 0 = bit 0, event 1 = bit 1, etc. The number of bits = number of events in this SCT. When the counter is used in bi-directional mode, it is possible to reverse the action specified by the output set and clear registers when counting down, See the OUTPUTCTRL register."]
+    #[doc = ""]
+    #[doc = ""]
+    #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CLR0` field.</div>"]
     #[inline(always)]
-    pub fn clr(&mut self) -> ClrW<OutClrSpec> {
+    pub fn clr(&mut self, n: u8) -> ClrW<OutClrSpec> {
+        #[allow(clippy::no_effect)]
+        [(); 16][n as usize];
+        ClrW::new(self, n)
+    }
+    #[doc = "Bit 0 - CLR0"]
+    #[inline(always)]
+    pub fn clr0(&mut self) -> ClrW<OutClrSpec> {
         ClrW::new(self, 0)
+    }
+    #[doc = "Bit 1 - CLR1"]
+    #[inline(always)]
+    pub fn clr1(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 1)
+    }
+    #[doc = "Bit 2 - CLR2"]
+    #[inline(always)]
+    pub fn clr2(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 2)
+    }
+    #[doc = "Bit 3 - CLR3"]
+    #[inline(always)]
+    pub fn clr3(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 3)
+    }
+    #[doc = "Bit 4 - CLR4"]
+    #[inline(always)]
+    pub fn clr4(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 4)
+    }
+    #[doc = "Bit 5 - CLR5"]
+    #[inline(always)]
+    pub fn clr5(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 5)
+    }
+    #[doc = "Bit 6 - CLR6"]
+    #[inline(always)]
+    pub fn clr6(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 6)
+    }
+    #[doc = "Bit 7 - CLR7"]
+    #[inline(always)]
+    pub fn clr7(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 7)
+    }
+    #[doc = "Bit 8 - CLR8"]
+    #[inline(always)]
+    pub fn clr8(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 8)
+    }
+    #[doc = "Bit 9 - CLR9"]
+    #[inline(always)]
+    pub fn clr9(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 9)
+    }
+    #[doc = "Bit 10 - CLR10"]
+    #[inline(always)]
+    pub fn clr10(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 10)
+    }
+    #[doc = "Bit 11 - CLR11"]
+    #[inline(always)]
+    pub fn clr11(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 11)
+    }
+    #[doc = "Bit 12 - CLR12"]
+    #[inline(always)]
+    pub fn clr12(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 12)
+    }
+    #[doc = "Bit 13 - CLR13"]
+    #[inline(always)]
+    pub fn clr13(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 13)
+    }
+    #[doc = "Bit 14 - CLR14"]
+    #[inline(always)]
+    pub fn clr14(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 14)
+    }
+    #[doc = "Bit 15 - CLR15"]
+    #[inline(always)]
+    pub fn clr15(&mut self) -> ClrW<OutClrSpec> {
+        ClrW::new(self, 15)
     }
 }
 #[doc = "SCT output 0 clear register\n\nYou can [`read`](crate::Reg::read) this register and get [`out_clr::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`out_clr::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
--- src/sct0/out/out_set.rs
+++ src/sct0/out/out_set.rs
@@ -6,11 +6,110 @@ pub type W = crate::W<OutSetSpec>;
 pub type SetR = crate::FieldReader<u16>;
 #[doc = "Field `SET` writer - A 1 in bit m selects event m to set output n (or clear it if SETCLRn = 0x1 or 0x2) output 0 = bit 0, output 1 = bit 1, etc. The number of bits = number of events in this SCT. When the counter is used in bi-directional mode, it is possible to reverse the action specified by the output set and clear registers when counting down, See the OUTPUTCTRL register."]
 pub type SetW<'a, REG> = crate::FieldWriter<'a, REG, 16, u16>;
+#[doc = "Field `OUT(0-15)` reader - "]
+pub type OutR = crate::BitReader;
+#[doc = "Field `OUT(0-15)` writer - "]
+pub type OutW<'a, REG> = crate::BitWriter<'a, REG>;
 impl R {
     #[doc = "Bits 0:15 - A 1 in bit m selects event m to set output n (or clear it if SETCLRn = 0x1 or 0x2) output 0 = bit 0, output 1 = bit 1, etc. The number of bits = number of events in this SCT. When the counter is used in bi-directional mode, it is possible to reverse the action specified by the output set and clear registers when counting down, See the OUTPUTCTRL register."]
     #[inline(always)]
     pub fn set_(&self) -> SetR {
         SetR::new((self.bits & 0xffff) as u16)
+    }
+    #[doc = ""]
+    #[doc = ""]
+    #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `OUT0` field.</div>"]
+    #[inline(always)]
+    pub fn out(&self, n: u8) -> OutR {
+        #[allow(clippy::no_effect)]
+        [(); 16][n as usize];
+        OutR::new(((self.bits >> n) & 1) != 0)
+    }
+    #[doc = "Iterator for array of:"]
+    #[doc = ""]
+    #[inline(always)]
+    pub fn out_iter(&self) -> impl Iterator<Item = OutR> + '_ {
+        (0..16).map(move |n| OutR::new(((self.bits >> n) & 1) != 0))
+    }
+    #[doc = "Bit 0 - OUT0"]
+    #[inline(always)]
+    pub fn out0(&self) -> OutR {
+        OutR::new((self.bits & 1) != 0)
+    }
+    #[doc = "Bit 1 - OUT1"]
+    #[inline(always)]
+    pub fn out1(&self) -> OutR {
+        OutR::new(((self.bits >> 1) & 1) != 0)
+    }
+    #[doc = "Bit 2 - OUT2"]
+    #[inline(always)]
+    pub fn out2(&self) -> OutR {
+        OutR::new(((self.bits >> 2) & 1) != 0)
+    }
+    #[doc = "Bit 3 - OUT3"]
+    #[inline(always)]
+    pub fn out3(&self) -> OutR {
+        OutR::new(((self.bits >> 3) & 1) != 0)
+    }
+    #[doc = "Bit 4 - OUT4"]
+    #[inline(always)]
+    pub fn out4(&self) -> OutR {
+        OutR::new(((self.bits >> 4) & 1) != 0)
+    }
+    #[doc = "Bit 5 - OUT5"]
+    #[inline(always)]
+    pub fn out5(&self) -> OutR {
+        OutR::new(((self.bits >> 5) & 1) != 0)
+    }
+    #[doc = "Bit 6 - OUT6"]
+    #[inline(always)]
+    pub fn out6(&self) -> OutR {
+        OutR::new(((self.bits >> 6) & 1) != 0)
+    }
+    #[doc = "Bit 7 - OUT7"]
+    #[inline(always)]
+    pub fn out7(&self) -> OutR {
+        OutR::new(((self.bits >> 7) & 1) != 0)
+    }
+    #[doc = "Bit 8 - OUT8"]
+    #[inline(always)]
+    pub fn out8(&self) -> OutR {
+        OutR::new(((self.bits >> 8) & 1) != 0)
+    }
+    #[doc = "Bit 9 - OUT9"]
+    #[inline(always)]
+    pub fn out9(&self) -> OutR {
+        OutR::new(((self.bits >> 9) & 1) != 0)
+    }
+    #[doc = "Bit 10 - OUT10"]
+    #[inline(always)]
+    pub fn out10(&self) -> OutR {
+        OutR::new(((self.bits >> 10) & 1) != 0)
+    }
+    #[doc = "Bit 11 - OUT11"]
+    #[inline(always)]
+    pub fn out11(&self) -> OutR {
+        OutR::new(((self.bits >> 11) & 1) != 0)
+    }
+    #[doc = "Bit 12 - OUT12"]
+    #[inline(always)]
+    pub fn out12(&self) -> OutR {
+        OutR::new(((self.bits >> 12) & 1) != 0)
+    }
+    #[doc = "Bit 13 - OUT13"]
+    #[inline(always)]
+    pub fn out13(&self) -> OutR {
+        OutR::new(((self.bits >> 13) & 1) != 0)
+    }
+    #[doc = "Bit 14 - OUT14"]
+    #[inline(always)]
+    pub fn out14(&self) -> OutR {
+        OutR::new(((self.bits >> 14) & 1) != 0)
+    }
+    #[doc = "Bit 15 - OUT15"]
+    #[inline(always)]
+    pub fn out15(&self) -> OutR {
+        OutR::new(((self.bits >> 15) & 1) != 0)
     }
 }
 #[cfg(feature = "debug")]
@@ -18,6 +117,22 @@ impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("OUT_SET")
             .field("set_", &self.set_())
+            .field("out0", &self.out0())
+            .field("out1", &self.out1())
+            .field("out2", &self.out2())
+            .field("out3", &self.out3())
+            .field("out4", &self.out4())
+            .field("out5", &self.out5())
+            .field("out6", &self.out6())
+            .field("out7", &self.out7())
+            .field("out8", &self.out8())
+            .field("out9", &self.out9())
+            .field("out10", &self.out10())
+            .field("out11", &self.out11())
+            .field("out12", &self.out12())
+            .field("out13", &self.out13())
+            .field("out14", &self.out14())
+            .field("out15", &self.out15())
             .finish()
     }
 }
@@ -26,6 +141,95 @@ impl W {
     #[inline(always)]
     pub fn set_(&mut self) -> SetW<OutSetSpec> {
         SetW::new(self, 0)
+    }
+    #[doc = ""]
+    #[doc = ""]
+    #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `OUT0` field.</div>"]
+    #[inline(always)]
+    pub fn out(&mut self, n: u8) -> OutW<OutSetSpec> {
+        #[allow(clippy::no_effect)]
+        [(); 16][n as usize];
+        OutW::new(self, n)
+    }
+    #[doc = "Bit 0 - OUT0"]
+    #[inline(always)]
+    pub fn out0(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 0)
+    }
+    #[doc = "Bit 1 - OUT1"]
+    #[inline(always)]
+    pub fn out1(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 1)
+    }
+    #[doc = "Bit 2 - OUT2"]
+    #[inline(always)]
+    pub fn out2(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 2)
+    }
+    #[doc = "Bit 3 - OUT3"]
+    #[inline(always)]
+    pub fn out3(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 3)
+    }
+    #[doc = "Bit 4 - OUT4"]
+    #[inline(always)]
+    pub fn out4(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 4)
+    }
+    #[doc = "Bit 5 - OUT5"]
+    #[inline(always)]
+    pub fn out5(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 5)
+    }
+    #[doc = "Bit 6 - OUT6"]
+    #[inline(always)]
+    pub fn out6(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 6)
+    }
+    #[doc = "Bit 7 - OUT7"]
+    #[inline(always)]
+    pub fn out7(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 7)
+    }
+    #[doc = "Bit 8 - OUT8"]
+    #[inline(always)]
+    pub fn out8(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 8)
+    }
+    #[doc = "Bit 9 - OUT9"]
+    #[inline(always)]
+    pub fn out9(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 9)
+    }
+    #[doc = "Bit 10 - OUT10"]
+    #[inline(always)]
+    pub fn out10(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 10)
+    }
+    #[doc = "Bit 11 - OUT11"]
+    #[inline(always)]
+    pub fn out11(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 11)
+    }
+    #[doc = "Bit 12 - OUT12"]
+    #[inline(always)]
+    pub fn out12(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 12)
+    }
+    #[doc = "Bit 13 - OUT13"]
+    #[inline(always)]
+    pub fn out13(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 13)
+    }
+    #[doc = "Bit 14 - OUT14"]
+    #[inline(always)]
+    pub fn out14(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 14)
+    }
+    #[doc = "Bit 15 - OUT15"]
+    #[inline(always)]
+    pub fn out15(&mut self) -> OutW<OutSetSpec> {
+        OutW::new(self, 15)
     }
 }
 #[doc = "SCT output 0 set register\n\nYou can [`read`](crate::Reg::read) this register and get [`out_set::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`out_set::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
